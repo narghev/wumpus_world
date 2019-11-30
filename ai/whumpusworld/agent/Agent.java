30f8c7a708d6cf6c342fca9af67eef37c86e2aa2
narghev <n.ghevandiani@gmail.com>
2019-11-30 20:05:59 +0400

    WIP agent move method

--- ai/whumpusworld/agent/Agent.java
+++ ai/whumpusworld/agent/Agent.java
@@ -10,19 +10,64 @@ import java.util.Queue;
 public class Agent {
     private Vector<Coordinate> steps;
     private Coordinate currentLocation;
+    private int currentLookingDirection;
+//    0 - left
+//    1 - up
+//    2 - right
+//    3 - down
 
     private GameMap agentMap;
     private boolean[][] visited;
     private Percept currentPercepts;
 
     public Agent(){
-        steps = new Vector<>(1,1);
         agentMap = new GameMap();
         visited = new boolean[4][4];
+        currentLocation = new Coordinate(0, 0);
+        steps = new Vector<>(1,1);
         currentPercepts = new Percept(false, false, false);
+        currentLookingDirection = 2;
+
         visited[0][0] = true;
         steps.addElement(new Coordinate(0, 0));
-        currentLocation = new Coordinate(0, 0);
+    }
+
+    private void moveUp() {
+        currentLookingDirection = 1;
+        currentLocation = new Coordinate(currentLocation.x, currentLocation.y + 1);
+    }
+
+    private void moveDown() {
+        currentLookingDirection = 3;
+        currentLocation = new Coordinate(currentLocation.x, currentLocation.y - 1);
+    }
+
+    private void moveRight() {
+        currentLookingDirection = 2;
+        currentLocation = new Coordinate(currentLocation.x + 1, currentLocation.y);
+    }
+
+    private void moveLeft() {
+        currentLookingDirection = 0;
+        currentLocation = new Coordinate(currentLocation.x - 1, currentLocation.y);
+    }
+
+    private void move(int direction) {
+        switch (direction) {
+            case 0:
+                moveLeft();
+                break;
+            case 1:
+                moveUp();
+                break;
+            case 2:
+                moveRight();
+                break;
+            case 3:
+                moveDown();
+                break;
+        }
+        steps.addElement(currentLocation);
     }
 
     private void updateKnowledgeBase() {
